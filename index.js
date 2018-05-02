2607d74522f936d8f525b9fbf4713ce6d9ebd5e5
hadfieldcj@gmail.com <hadfieldcj@gmail.com>
2018-05-02 04:47:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return 'Happy holidays, ${name}!'
 }
 
 function happyHolidayTo(holiday,name) {
   return "Happy ${holiday}, ${name}!"
 }
 
-function holidayCountdown(name,days) {
+function holidayCountdown(holiday,days) {
   return "It's ${days} until ${holiday}"
 }
